82ffdb01c10da7e4330d02f504d12ab09ad47733
draftsman-io <system@draftsman.io>
2023-10-24 17:22:12 +0000

    D1562559209:anove:Demo

---
+++ js/configuration.js
@@ -0,0 +1,5 @@
+localStorage["aws-congnito-user-pool-id"] = "eu-central-1_Cyyh4oLjM";
+localStorage["aws-congnito-app-id"] = "2v1p8l7rdjv7ctn9hnj01rssim";
+const api_url = "https://xsjdrhw47vcepbvy2byajsi3hq.appsync-api.eu-central-1.amazonaws.com/graphql";
+const api_ws = "wss://xsjdrhw47vcepbvy2byajsi3hq.appsync-realtime-api.eu-central-1.amazonaws.com/graphql";
+const api_key = "da2-mpwfe2yc2nf5hiibmuelsssigm";
