9961dc1fb4af2b979b19a8e641a9bde330ec48d4
draftsman-io <system@draftsman.io>
2024-03-03 21:27:09 +0000

    D1562559209:anove:Demo

--- js/configuration-staging.js
+++ js/configuration-staging.js
@@ -1,7 +1,7 @@
 if (localStorage["staging-environment"] && localStorage["staging-environment"] == "true"){
 	console.log("Connected to staging")
 	localStorage["aws-congnito-user-pool-id"] = "eu-central-1_yVRR5Nfwu";
-	localStorage["aws-congnito-app-id"] = "3s8u869v3o71l5kthvkfnqlbpo";
+	localStorage["aws-congnito-app-id"] = "1l5rq3us53vuium716kpvq9tlk";
 	api_url = "https://pcvj26uqyveutmhjlr6aklchuu.appsync-api.eu-central-1.amazonaws.com/graphql";
 	api_ws = "wss://pcvj26uqyveutmhjlr6aklchuu.appsync-realtime-api.eu-central-1.amazonaws.com/graphql";
 	api_key = "da2-awjw7uh72jhahh74gnkc7nlmsm";
