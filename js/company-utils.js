a49fd51255a578354f27b80b7dbcb3e212ad39fc
bohanssen <bohanssen@gmail.com>
2024-03-26 01:03:55 +0100

    control scherm begin gemaakt

--- js/company-utils.js
+++ js/company-utils.js
@@ -1,4 +1,42 @@
 
+const requirements_query = `
+query MyQuery {
+  Company {
+    get(key: "company:draftsman") {
+      controlframework {
+        requirements {
+          arn
+          name
+          summary
+        }
+        arn
+        name
+      }
+    }
+  }
+}
+`;
+
+async function list_requirements(){
+    let requirements = {};
+    let data = await Draftsman.query(requirements_query,{},false);
+    data = data["Company"]["get"]["controlframework"];
+    data.forEach(framework => {
+        framework.requirements.forEach(requirement => {
+            requirements[requirement.arn] = {
+                name: requirement.name,
+                framework: framework.name
+            };
+        });
+    });
+    return requirements;
+}
+
+async function list_frameworks(){
+    let data = await Draftsman.query(requirements_query,{},false);
+    return data["Company"]["get"]["controlframework"];
+}
+
 const query_string = `
 query Search($searchString: String = "") {
   Profile {
@@ -34,4 +72,23 @@ async function get_frameworks(selected_frameworks){
     let query = document.getElementById("framework-list").innerText;
     let data = await Draftsman.query(query,{},false);
     return data["ControlFramework"]["filter"]["resultset"].filter(x => !arns.includes(x.arn));
+}
+
+var periodicity_templates = [];
+var periodicity_names = {};
+
+for (i = 1; i < 31; i++){
+    let id = i + "D";
+    periodicity_templates.push(id);
+    periodicity_names[id] = i + " Days"
+}
+for (i = 1; i < 12; i++){
+    let id = i + "M";
+    periodicity_templates.push(id);
+    periodicity_names[id] = i + " Months"
+}
+for (i = 1; i < 6; i++){
+    let id = i + "Y";
+    periodicity_templates.push(id);
+    periodicity_names[id] = i + " Years"
 }
