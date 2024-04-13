fff1579be6e69da6272175e6de84e0356a0127be
draftsman-io <system@draftsman.io>
2024-04-13 06:22:33 +0000

    D1562559209:anove:Demo

--- js/configuration.js
+++ js/configuration.js
@@ -8,8 +8,8 @@ if (!localStorage["staging-environment"] || localStorage["staging-environment"] 
 	console.log("Connected to production");
 	stage = "production";
 	bucket = "d33lj2wpoi7g8j.cloudfront.net";
-	localStorage["aws-congnito-user-pool-id"] = "eu-central-1_Cyyh4oLjM";
-	localStorage["aws-congnito-app-id"] = "2v1p8l7rdjv7ctn9hnj01rssim";
+	localStorage["aws-congnito-user-pool-id"] = "eu-central-1_7jQbIovzx";
+	localStorage["aws-congnito-app-id"] = "fag4r17dojji41l9kv736uv47";
 	localStorage["aws-congnito-ui"] = "https://demo-production.auth.eu-central-1.amazoncognito.com";	api_url = "https://xsjdrhw47vcepbvy2byajsi3hq.appsync-api.eu-central-1.amazonaws.com/graphql";
 	api_ws = "wss://xsjdrhw47vcepbvy2byajsi3hq.appsync-realtime-api.eu-central-1.amazonaws.com/graphql";
 	api_key = "da2-mpwfe2yc2nf5hiibmuelsssigm";
