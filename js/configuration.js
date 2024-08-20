edd74f70add14b179dc5e0c7534370ac9c5b0918
bohanssen <bohanssen@gmail.com>
2024-08-20 09:27:51 +0200

    Update configuration.js

--- js/configuration.js
+++ js/configuration.js
@@ -10,7 +10,7 @@ if (!localStorage["staging-environment"] || localStorage["staging-environment"] 
 	bucket = "d33lj2wpoi7g8j.cloudfront.net";
 	localStorage["aws-congnito-user-pool-id"] = "eu-central-1_7jQbIovzx";
 	localStorage["aws-congnito-app-id"] = "fag4r17dojji41l9kv736uv47";
-	localStorage["aws-congnito-ui"] = "https://fag4r17dojji41l9kv736uv47.auth.eu-central-1.amazoncognito.com";	api_url = "https://xsjdrhw47vcepbvy2byajsi3hq.appsync-api.eu-central-1.amazonaws.com/graphql";
+	localStorage["aws-congnito-ui"] = "https://demo-production.auth.eu-central-1.amazoncognito.com";	api_url = "https://xsjdrhw47vcepbvy2byajsi3hq.appsync-api.eu-central-1.amazonaws.com/graphql";
 	api_ws = "wss://xsjdrhw47vcepbvy2byajsi3hq.appsync-realtime-api.eu-central-1.amazonaws.com/graphql";
 	api_key = "da2-mpwfe2yc2nf5hiibmuelsssigm";
 }
