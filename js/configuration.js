c0946bac68a6dca7a7130048c0c35e187f323c61
bohanssen <bohanssen@gmail.com>
2024-10-08 01:24:59 +0200

    Update configuration.js

--- js/configuration.js
+++ js/configuration.js
@@ -7,10 +7,11 @@ var stage = ""
 if (!localStorage["staging-environment"] || localStorage["staging-environment"] == "false"){
 	console.log("Connected to production");
 	stage = "production";
-	bucket = "d33lj2wpoi7g8j.cloudfront.net";
+	bucket = "d2ybnbzqf2uein.cloudfront.net";
 	localStorage["aws-congnito-user-pool-id"] = "eu-central-1_7jQbIovzx";
 	localStorage["aws-congnito-app-id"] = "fag4r17dojji41l9kv736uv47"; 
-	localStorage["aws-congnito-ui"] = "https://demo-production.auth.eu-central-1.amazoncognito.com";	api_url = "https://xsjdrhw47vcepbvy2byajsi3hq.appsync-api.eu-central-1.amazonaws.com/graphql";
-	api_ws = "wss://xsjdrhw47vcepbvy2byajsi3hq.appsync-realtime-api.eu-central-1.amazonaws.com/graphql";
-	api_key = "da2-mpwfe2yc2nf5hiibmuelsssigm";
+	localStorage["aws-congnito-ui"] = "https://demo-production.auth.eu-central-1.amazoncognito.com";	
+	api_url = "https://pejulblnazbqng3u5yblamjqjm.appsync-api.eu-central-1.amazonaws.com/graphql";
+	api_ws = "wss://pejulblnazbqng3u5yblamjqjm.appsync-realtime-api.eu-central-1.amazonaws.com/graphql";
+	api_key = "da2-77xu7vregvbdrn4kybeczl6dzm";
 }
