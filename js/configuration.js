cdd8965ccf0b49a604339d06b343919f503669a2
draftsman-io <system@draftsman.io>
2024-10-08 22:27:47 +0000

    D1562559209:anove:Demo

--- js/configuration.js
+++ js/configuration.js
@@ -9,9 +9,8 @@ if (!localStorage["staging-environment"] || localStorage["staging-environment"] 
 	stage = "production";
 	bucket = "d2ybnbzqf2uein.cloudfront.net";
 	localStorage["aws-congnito-user-pool-id"] = "eu-central-1_7jQbIovzx";
-	localStorage["aws-congnito-app-id"] = "fag4r17dojji41l9kv736uv47"; 
-	localStorage["aws-congnito-ui"] = "https://demo-production.auth.eu-central-1.amazoncognito.com";	
-	api_url = "https://pejulblnazbqng3u5yblamjqjm.appsync-api.eu-central-1.amazonaws.com/graphql";
+	localStorage["aws-congnito-app-id"] = "fag4r17dojji41l9kv736uv47";
+	localStorage["aws-congnito-ui"] = "https://demo-production.auth.eu-central-1.amazoncognito.com";	api_url = "https://pejulblnazbqng3u5yblamjqjm.appsync-api.eu-central-1.amazonaws.com/graphql";
 	api_ws = "wss://pejulblnazbqng3u5yblamjqjm.appsync-realtime-api.eu-central-1.amazonaws.com/graphql";
 	api_key = "da2-77xu7vregvbdrn4kybeczl6dzm";
 }
